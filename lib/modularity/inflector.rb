5b625a06edbe78e226c1c1043eb4865dfaf1a4c0
Tobias Kraze <tobias@kraze.eu>
2011-07-23 22:18:01 +0200

    Ruby 1.9 compatibility

--- lib/modularity/inflector.rb
+++ lib/modularity/inflector.rb
@@ -13,7 +13,25 @@ module Modularity
         end
       end
 
-      # File activesupport/lib/active_support/inflector.rb, line 355
+      
+    if Module.method(:const_get).arity == 1
+      # Tries to find a constant with the name specified in the argument string:
+      #
+      #   "Module".constantize     # => Module
+      #   "Test::Unit".constantize # => Test::Unit
+      #
+      # The name is assumed to be the one of a top-level constant, no matter whether
+      # it starts with "::" or not. No lexical context is taken into account:
+      #
+      #   C = 'outside'
+      #   module M
+      #     C = 'inside'
+      #     C               # => 'inside'
+      #     "C".constantize # => 'outside', same as ::C
+      #   end
+      #
+      # NameError is raised when the name is not in CamelCase or the constant is
+      # unknown.
       def constantize(camel_cased_word)
         names = camel_cased_word.split('::')
         names.shift if names.empty? || names.first.empty?
@@ -24,6 +42,18 @@ module Modularity
         end
         constant
       end
+    else
+      def constantize(camel_cased_word) #:nodoc:
+        names = camel_cased_word.split('::')
+        names.shift if names.empty? || names.first.empty?
+
+        constant = Object
+        names.each do |name|
+          constant = constant.const_defined?(name, false) ? constant.const_get(name) : constant.const_missing(name)
+        end
+        constant
+      end
+    end
 
     end
   end
--- modularity.gemspec
+++ modularity.gemspec
@@ -5,11 +5,11 @@
 
 Gem::Specification.new do |s|
   s.name = %q{modularity}
-  s.version = "0.5.0"
+  s.version = "0.6.0"
 
   s.required_rubygems_version = Gem::Requirement.new(">= 0") if s.respond_to? :required_rubygems_version=
   s.authors = ["Henning Koch"]
-  s.date = %q{2010-02-15}
+  s.date = %q{2010-02-18}
   s.description = %q{Traits and partial classes for Ruby}
   s.email = %q{github@makandra.de}
   s.extra_rdoc_files = [
