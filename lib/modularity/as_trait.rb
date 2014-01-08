c72700673c7f312165b5c3545dd593d8adefec82
Henning Koch <henning.koch@makandra.de>
2014-01-08 13:53:06 +0100

    Modularity 2 syntax

--- lib/modularity/as_trait.rb
+++ lib/modularity/as_trait.rb
@@ -1,9 +1,40 @@
 module Modularity
-  module AsTrait
-    def as_trait(&block)
-      @trait_macro = block
+
+  class ParametrizedTrait < Module
+
+    def initialize(blank_trait, args)
+      @args = args
+      @macro = blank_trait.instance_variable_get(:@modularity_macro)
+      include(blank_trait)
     end
+
+    def included(base)
+      base.class_exec(*@args, &@macro)
+    end
+
+  end
+
+  module AsTrait
+
+    def as_trait(&macro)
+
+      @modularity_macro = macro
+
+      def self.included(base)
+        unless base.is_a?(ParametrizedTrait)
+          base.class_exec(&@modularity_macro)
+        end
+
+      end
+
+      def self.[](*args)
+        blank_trait = self
+        ParametrizedTrait.new(blank_trait, args)
+      end
+
+    end
+
   end
 end
 
-Object.send :include, Modularity::AsTrait
+Module.send(:include, Modularity::AsTrait)
--- spec/as_trait_spec.rb
+++ spec/as_trait_spec.rb
@@ -1,29 +1,116 @@
 require 'spec_helper'
 
-module Trait
-  as_trait do
-    "hi world"
-  end
-end
-
-module ParametrizedTrait
-  as_trait do |name|
-    "hi, #{name}"
-  end
-end
-
 describe Modularity::AsTrait do
 
-  describe 'as_trait' do
-
-    it "should let modules save a proc upon loading" do
-      Trait.instance_variable_get("@trait_macro").call.should == "hi world"
+  describe '.included' do
+    
+    before :each do
+      @doing_class = Class.new
     end
 
-    it "should let modules save a proc with parameters upon loading" do
-      ParametrizedTrait.instance_variable_get("@trait_macro").call("jean").should == "hi, jean"
+    describe 'without parameters' do
+  
+      it "applies the trait macro of the given module" do
+
+        module DoesSome
+          as_trait do
+            some_trait_included
+          end
+        end
+
+        @doing_class.should_receive(:some_trait_included)
+
+        @doing_class.class_eval do
+          include DoesSome
+        end
+
+      end
+
+      it "applies the trait macro of the given namespaced module" do
+
+        module Some
+          module DoesChild
+            as_trait do
+              some_child_trait_included
+            end
+          end
+        end
+
+        @doing_class.should_receive(:some_child_trait_included)
+
+        @doing_class.class_eval do
+          include Some::DoesChild
+        end
+
+      end
+
+      it "lets a trait define methods with different visibility" do
+
+        module DoesVisibility
+          as_trait do
+            def public_method_from_trait
+            end
+            protected
+            def protected_method_from_trait
+            end
+            private
+            def private_method_from_trait
+            end
+          end
+        end
+
+        @doing_class.class_eval do
+          include DoesVisibility
+        end
+
+        instance = @doing_class.new
+
+        instance.public_methods.collect(&:to_s).should include("public_method_from_trait")
+        instance.protected_methods.collect(&:to_s).should include("protected_method_from_trait")
+        instance.private_methods.collect(&:to_s).should include("private_method_from_trait")
+
+      end
+
+    end
+
+    describe "with parameters" do
+
+      it "it applies a trait macro with parameters" do
+
+        module DoesCallMethod
+          as_trait do |field|
+            send(field)
+          end
+        end
+
+        @doing_class.should_receive(:foo)
+        @doing_class.class_eval do
+          include DoesCallMethod[:foo]
+        end
+
+      end
+
+      it "facilitates metaprogramming acrobatics" do
+
+        module DoesDefineConstantMethod
+          as_trait do |name, return_value|
+            define_method name do
+              return_value
+            end
+          end
+        end
+
+        @doing_class.class_eval do
+          include DoesDefineConstantMethod["some_method", "some_return_value"]
+        end
+
+        instance = @doing_class.new
+        instance.should respond_to(:some_method)
+        instance.some_method.should == "some_return_value"
+      end
+
     end
 
   end
-  
+
 end
