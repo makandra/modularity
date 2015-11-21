ecd605b95f09b41979464c0e1ae7dd1b88621ff8
Your Name <you@example.com>
2015-11-21 05:40:59 -0500

    Support for prepend.

--- lib/modularity/as_trait.rb
+++ lib/modularity/as_trait.rb
@@ -12,6 +12,11 @@ module Modularity
       base.class_exec(*@args, &@macro)
     end
 
+    def prepended(base)
+      mod  = Module.new
+      mod.module_exec(*@args, &@macro)
+      base.prepend mod
+    end
   end
 
   module AsTrait
@@ -20,20 +25,27 @@ module Modularity
 
       @modularity_macro = macro
 
+      def self.prepended(base)
+        unless base.is_a?(ParametrizedTrait)
+          mod = Module.new
+          mod.module_exec(*@args, &@macro)
+          base.prepend mod      
+        end
+      end
+      
       def self.included(base)
         unless base.is_a?(ParametrizedTrait)
           base.class_exec(&@modularity_macro)
         end
-
+        
       end
 
       def self.[](*args)
         blank_trait = self
         ParametrizedTrait.new(blank_trait, args)
       end
-
+      
     end
-
   end
 end
 
